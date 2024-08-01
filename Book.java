b999bd6db913ec3bb2193c1415e1661bd02265d9
Aneesh Ramanathan <147331581+aneeshramanathan@users.noreply.github.com>
2024-07-31 22:00:03 -0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class Book implements Comparable<Book>, Serializable{
+    
+    @Override
+    public int compareTo(Book o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+    
+}
